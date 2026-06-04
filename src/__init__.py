fd4911c67bab3e3a8148ff3e6f978ae68c8ce430
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-06-05 00:49:15 +0530

    Add environment configuration and refactor database initialization logic

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,9 @@
 from .database import init_database
+from uuid import uuid4
+from dotenv import load_dotenv
 
 
 def start_crawler():
+    load_dotenv()
     init_database()
+    crawl_id = str(uuid4())
--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,10 +1,16 @@
 from enum import Enum
+import os
 from typing import Any, get_args, get_origin
 
 from .models import Sources, Hostnames, Urls, Content, CrawlRun
 from .db_builder import AppwriteSchemaBuilder
 from appwrite.client import Client
 from pydantic_core import PydanticUndefined
+
+APPWRITE_ENDPOINT_ENV = "APPWRITE_ENDPOINT"
+APPWRITE_PROJECT_ID_ENV = "APPWRITE_PROJECT_ID"
+APPWRITE_API_KEY_ENV = "APPWRITE_API_KEY"
+APPWRITE_DATABASE_ID_ENV = "APPWRITE_DATABASE_ID"
 
 
 def _unwrap_optional(annotation):
@@ -68,21 +74,27 @@ def _model_to_collection_schema(model) -> dict[str, Any]:
     }
 
 
-appwrite_client = (
-    Client()
-    .set_endpoint("https://fra.stage.cloud.appwrite.io/v1")
-    .set_project("6a21bbda001db9ed5e4b")
-    .set_key(
-        "standard_93ccf0bd056bb9f88b7317523df35016b48d4d0ae61abbfedb39a6a9919e2c06d2a9dd22015af33b9c064273a94e0c4e0bbe232b21f37ca61aa9ff4c88a53b17864dcc4541a7141e05f7f870688299a1b97a8750a5e3987d5fef15a1cc6b4e97d250c47e881284f4e292cb6a494d8f88a785daca0b301cc8fb89883e5d62ca23"
+def _create_appwrite_client() -> Client:
+    return (
+        Client()
+        .set_endpoint(os.environ.get(APPWRITE_ENDPOINT_ENV))
+        .set_project(os.environ.get(APPWRITE_PROJECT_ID_ENV))
+        .set_key(os.environ.get(APPWRITE_API_KEY_ENV))
     )
-)
+
+
+def _create_schema_builder() -> AppwriteSchemaBuilder:
+    return AppwriteSchemaBuilder(
+        _create_appwrite_client(),
+        database_id=os.environ.get(APPWRITE_DATABASE_ID_ENV),
+    )
 
 
 def init_database():
-    db = AppwriteSchemaBuilder(appwrite_client, database_id="feedx")
+    db = _create_schema_builder()
     for model in [Sources, Hostnames, Urls, Content, CrawlRun]:
         db.create_collection_from_dict(_model_to_collection_schema(model))
 
 
 def get_database():
-    return AppwriteSchemaBuilder(appwrite_client, database_id="feedx").get_database()
+    return _create_schema_builder().get_database()
