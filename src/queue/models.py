9eec8c627edb1ef09a1bc46aaba0e36699f62700
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-06-13 00:35:45 +0530

    updated models and added scheduler queue

--- src/queue/models.py
+++ src/queue/models.py
@@ -1,4 +1,5 @@
-from ..database.models import URL, Hostname, CrawlState
+from ..database.models import URL, Hostname
+from datetime import datetime, timezone, timedelta
 
 
 class URLRow(URL):
@@ -9,3 +10,31 @@ class URLRow(URL):
 class HostnameRow(Hostname):
     id: str
     sequence: int
+
+
+class SchedulerQueueItem:
+    def __init__(
+        self,
+        hostname: str,
+        next_allowed_at: datetime | None,
+    ):
+        self.hostname = hostname
+        self.next_allowed_at = (
+            next_allowed_at
+            if next_allowed_at is not None
+            else datetime.now(timezone.utc)
+        )
+
+    def __lt__(self, other: "SchedulerQueueItem") -> bool:
+        return self.next_allowed_at < other.next_allowed_at
+
+    def __repr__(self) -> str:
+        return (
+            "SchedulerQueueItem("
+            f"hostname={self.hostname!r}, "
+            f"next_allowed_at={self.next_allowed_at!r}"
+            ")"
+        )
+
+    def add_seconds(self, seconds: int):
+        self.next_allowed_at += timedelta(seconds=seconds)
---
+++ src/queue/scheduler_queue.py
@@ -0,0 +1,40 @@
+import heapq
+from .models import SchedulerQueueItem
+from ..database import get_database, APPWRITE_DATABASE_ID
+from ..database.models import Hostname
+from appwrite.query import Query
+from scout.logger import get_logger
+
+
+class SchedulerQueue:
+    def __init__(self):
+        self._queue: list[SchedulerQueueItem] = []
+        self._logger = get_logger("SchedulerQueue")
+
+    def init(self, hostnames: list[str]):
+        try:
+            self._queue = heapq.heapify(self._get_hostname_items(hostnames))
+            self._logger.info(f"Pushed {len(hostnames)} urls", tag="INIT")
+        except Exception as e:
+            self._logger.error(f"Error", tag="INIT", error=e)
+
+    def push(self, item: SchedulerQueueItem):
+        heapq.heappush(self._queue, item)
+
+    def pop(self):
+        return heapq.heappop(self._queue)
+
+    def _get_hostname_items(self, hostnames: list[str]):
+        database = get_database()
+        queries = [
+            Query.equal("name", hostnames),
+            Query.select(["name", "next_allowed_at"]),
+        ]
+        rows = database.list_rows(APPWRITE_DATABASE_ID, Hostname.__name__, queries)
+        return [
+            SchedulerQueueItem(
+                hostname=row.model_dump().get("name"),
+                next_allowed_at=row.model_dump().get("next_allowed_at"),
+            )
+            for row in rows.rows
+        ]
