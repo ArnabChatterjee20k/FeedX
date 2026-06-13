eab3d392da450920e7536dae2329c78ec38ea65d
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-06-14 00:46:41 +0530

    updated back queue to have async primitive for the pop with critical section

--- src/queue/back_queue.py
+++ src/queue/back_queue.py
@@ -2,12 +2,14 @@ from collections import deque
 from scout.logger import get_logger
 from .models import URLRow
 from . import Queue
+import asyncio
 
 
 class BackQueue(Queue):
     def __init__(self):
         self._queues: dict[str, deque[URLRow]] = {}
         self._logger = get_logger("BACK_QUEUE")
+        self._hostname_lock: dict[str, asyncio.Lock] = {}
 
     def init(self, urls: list[URLRow]):
         for url in urls:
@@ -18,6 +20,7 @@ class BackQueue(Queue):
     def push(self, hostname: str, url: URLRow):
         if hostname in self._queues:
             self._queues[hostname].append(url)
+            self._hostname_lock[hostname] = asyncio.Lock()
         else:
             self._queues[hostname] = deque([url])
 
@@ -25,6 +28,12 @@ class BackQueue(Queue):
         if hostname not in self._queues or not self._queues[hostname]:
             return None
         return self._queues[hostname].popleft()
+
+    async def pop_async(self, hostname):
+        if hostname not in self._queues or not self._queues[hostname]:
+            return None
+        async with self._hostname_lock[hostname]:
+            return self.pop(hostname)
 
     def get_hostnames(self) -> list[str]:
         return list(self._queues.keys())
