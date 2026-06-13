3d0b60a6dcfc09e461a7b257fefd3511e51da5d8
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-06-14 00:46:36 +0530

    updated scheduler queue to have async primitive for the pop with critical section

--- src/queue/scheduler_queue.py
+++ src/queue/scheduler_queue.py
@@ -6,12 +6,14 @@ from appwrite.query import Query
 from scout.logger import get_logger
 from datetime import datetime, timezone
 from . import Queue
+import asyncio
 
 
 class SchedulerQueue(Queue):
     def __init__(self):
         self._queue: list[SchedulerQueueItem] = []
         self._logger = get_logger("SchedulerQueue")
+        self._hostname_available_condition = asyncio.Condition()
 
     def init(self, hostnames: list[str]):
         try:
@@ -26,10 +28,60 @@ class SchedulerQueue(Queue):
     def push(self, item: SchedulerQueueItem):
         heapq.heappush(self._queue, item)
 
+    async def push_async(self, item: SchedulerQueueItem):
+        async with self._hostname_available_condition:
+            self.push(item)
+            await self._hostname_available_condition.notify()
+
     def pop(self):
         if not self._queue:
             return None
         return heapq.heappop(self._queue)
+
+    async def pop_async(self, timeout=5):
+        try:
+            async with self._hostname_available_condition:
+                await asyncio.wait_for(
+                    self._hostname_available_condition.wait_for(
+                        lambda: len(self._queue) > 0
+                    ),
+                    timeout=timeout,
+                )
+
+                if self._queue[0].next_allowed_at <= datetime.now(timezone.utc):
+                    return self.pop()
+
+                # not using the condition notify here cause what if notify in that case isn't called? better to use sleep since its totally based on duration
+                # wait_timeout = min(
+                #     timeout,
+                #     (
+                #         self._queue[0].next_allowed_at - datetime.now(timezone.utc)
+                #     ).total_seconds(),
+                # )
+                # await asyncio.wait_for(
+                #     self._hostname_available_condition.wait_for(
+                #         lambda: self._queue[0].next_allowed_at
+                #         <= datetime.now(timezone.utc)
+                #     ),
+                #     timeout=wait_timeout,
+                # )
+                # return self.pop()
+
+                # popping it so that its not get used by the other worker
+                # if any issues happened then the database already has the item in current state and can be peaked
+                item_for_the_current_worker = self.pop()
+                delay = (
+                    self._queue[0].next_allowed_at - datetime.now(timezone.utc)
+                ).total_seconds()
+
+            # releasing lock so that the item isn't blocked
+            if delay > timeout:
+                return None
+            asyncio.sleep(delay)
+            return item_for_the_current_worker
+
+        except asyncio.TimeoutError:
+            return None
 
     def _get_hostname_items(self, hostnames: list[str]):
         database = get_database()
